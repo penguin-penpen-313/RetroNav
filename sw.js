7537dedc1893dc42fb93e96c32317fa633116c73
penguin-penpen-313 <penguinpnpn@gmail.com>
2026-03-15 03:40:40 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,3 +1,4 @@
+// バージョンを変更するだけで古いキャッシュが自動削除されます
 const CACHE_NAME = 'retronav-v2';
 const ASSETS = [
   './',
@@ -7,12 +8,29 @@ const ASSETS = [
   './icon-512.png'
 ];
 
+// インストール時: 新しいキャッシュを作成し、即座にアクティブ化
 self.addEventListener('install', e => {
   e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(ASSETS))
+      .then(() => self.skipWaiting()) // 待機せず即アクティブ化
   );
 });
 
+// アクティブ化時: 自分以外の古いキャッシュを全て削除
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    ).then(() => self.clients.claim()) // 開いているページをすぐ制御下に
+  );
+});
+
+// フェッチ: キャッシュ優先、なければネットワーク
 self.addEventListener('fetch', e => {
   e.respondWith(
     caches.match(e.request).then(r => r || fetch(e.request))
